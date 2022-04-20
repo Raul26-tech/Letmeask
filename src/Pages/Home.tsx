4f928dd6d2852ae54fe0a6eaa8f3d5326a3e5daf
Raul26-tech <rauletec2020@gmail.com>
2022-04-20 15:32:18 -0300

    finalizando pagina

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -2,6 +2,8 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from "../assets/images/logo.svg"
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { Button } from '../components/Button'
+
 import '../styles/auth.css'
 
 export function Home(){
@@ -15,21 +17,21 @@ export function Home(){
                 <p>Aprenda e compartilhe conhecimento com outras pessoas</p>
             </aside>
             <main>
-                <div className="div">
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button>
+                    <button className='create-room'>
                         <img src={googleIconImg} alt="" />
                         Crie sua sala com o google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className='separator'>ou entre em uma sala</div>
                     <form>
                         <input 
                             type="text" 
                             placeholder='Digite o código da sala'
                         />
-                        <button type='submit'>
-                            Entar na sala
-                        </button>
+                        <Button type='submit'>
+                            Entrar na sala
+                        </Button>
                     </form>
                 </div>
             </main>
